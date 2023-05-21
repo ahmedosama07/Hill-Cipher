e8c418cf70078ab82bba6a5e2bd34e1e17a3987b
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-22 00:54:54 +0300

    Create letters2num.m

---
+++ letters2num.m
@@ -0,0 +1,6 @@
+function num = letters2num(word)
+%letters2num Summary of this function goes here
+%   function returns the numerical value of letters
+num = double(upper(word)) - double('A');
+end
+
