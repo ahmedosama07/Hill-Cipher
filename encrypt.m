9bfdea975c0341df720830e72fb4958e6fee151d
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-22 03:22:14 +0300

    Create encrypt.m

---
+++ encrypt.m
@@ -0,0 +1,19 @@
+function [c, k] = encrypt(text)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+plainText = cleanText(text);
+p = letters2num(plainText);
+factors = factor(size(p, 2));
+m = factors(1);
+n = size(p, 2) / m;
+k = keyGen(m);
+base = 26;
+
+c = zeros(size(p));
+
+for i = 1 : n
+    tmp = mod(p(((i - 1) * m + 1) : i * m) * k, base);
+    c(((i - 1) * m + 1) : i * m) = tmp;
+end
+c = nums2letters(c);
+end
