a49aa20d192aabc09059272935c1125e63f65586
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-05-23 21:53:24 +0300

    Solve for decrypting problem

--- keyGen.m
+++ keyGen.m
@@ -2,9 +2,10 @@ function key = keyGen(n)
 %keyGen Summary of this function goes here
 %   key generator function
 base = 26;
-t = 1;
+t = 0;
 while t ~= 1
-    key = randi([0 26], n);
-    t = gcd(det(key), base);
+    k = randi([0 26], n);
+    t = gcd(int32(det(k)), base);
 end
+key = k;
 end
